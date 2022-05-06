c3dd3794debd502a676ac7556fa41d92d3063372
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-05-06 15:29:45 -0300

    refac:espaçamento preetierr

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from 'react';
 import { ContainerButton } from './style';
 
 interface ButtonProps {
-    children: ReactNode;
-    onClick: () => void;
+  children: ReactNode;
+  onClick: () => void;
 }
 
 export const Button = ({ children, onClick }: ButtonProps) => {
-    return (
-        <ContainerButton>
-            <button onClick={onClick}>{children}</button>
-        </ContainerButton>
-    );
+  return (
+    <ContainerButton>
+      <button onClick={onClick}>{children}</button>
+    </ContainerButton>
+  );
 };
